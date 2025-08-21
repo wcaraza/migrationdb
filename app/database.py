26c8a19c1eb6ba6fbbaedc00f091d90b9c7d59ce
Walter Caraza <waltercaraza@Walters-MacBook-Pro.local>
2025-08-21 15:28:09 +0200

    adding endpoint to get metrics for hired employees by quarter and filtered by year

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 import os
 from pathlib import Path
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from urllib.parse import quote_plus
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, Session
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,3 +20,22 @@ DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{D
 #print(DATABASE_URL)
 engine = create_engine(DATABASE_URL, future=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
+
+
+def employee_quarter(year: int, db: Session):
+    query = text(f"""
+        SELECT d.department,
+               j.job,
+               SUM(CASE WHEN EXTRACT(QUARTER FROM h.datetime) = 1 THEN 1 ELSE 0 END) AS Q1,
+               SUM(CASE WHEN EXTRACT(QUARTER FROM h.datetime) = 2 THEN 1 ELSE 0 END) AS Q2,
+               SUM(CASE WHEN EXTRACT(QUARTER FROM h.datetime) = 3 THEN 1 ELSE 0 END) AS Q3,
+               SUM(CASE WHEN EXTRACT(QUARTER FROM h.datetime) = 4 THEN 1 ELSE 0 END) AS Q4
+        FROM employees h
+        JOIN departments d ON h.department_id = d.id
+        JOIN jobs j ON h.job_id = j.id
+        WHERE EXTRACT(YEAR FROM h.datetime) = {year}
+        GROUP BY d.department, j.job
+        ORDER BY d.department, j.job
+    """)
+    result = db.execute(query).mappings().all()
+    return result
--- app/main.py
+++ app/main.py
@@ -2,19 +2,18 @@ from __future__ import annotations
 import os
 from pathlib import Path
 
-from fastapi import FastAPI, UploadFile, HTTPException, Depends
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
 from dotenv import load_dotenv
 
 load_dotenv()
 
-from .database import SessionLocal, engine
+from .database import SessionLocal, engine, employee_quarter
 from .etl import collect_dir_csvs, ingest_files_in_order
 
 
 
-app = FastAPI(title="DB Migration REST API", version="1.2.0")
+app = FastAPI(title="DB Migration REST API", version="1.3.0")
 
 
 
@@ -26,8 +25,8 @@ def get_db():
         db.close()
 
 
-@app.post("/ingest/csv/from-dir")
-def ingest_from_dir(
+@app.post("/migration/csv-from-dir")
+def migration_from_dir(
     db: Session = Depends(get_db)):
     dir_env = os.getenv("INGEST_DIR")
     dir_path = Path(dir_env)
@@ -37,3 +36,8 @@ def ingest_from_dir(
     print(files)
     results = ingest_files_in_order(db, files)
     return results
+
+@app.get("/metrics/employees-quarter-by-year")
+def metrics_employee_quarter_by_year(year: int, 
+                                db: Session = Depends(get_db)):
+    return employee_quarter(year,db)
